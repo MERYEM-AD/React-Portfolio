824e99a9e245f4d84f28ab2a38f401d68043be56
MERYEM-AD <adiri.meryem.mw@gmail.com>
2022-02-15 00:46:54 -0500

    Updates

---
+++ precache-manifest.d3f89e1e4fa72ed49469c0b445a34442.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4aaf2d07db1e8857c107053dab16cec",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9bdffbe546bb673795e5",
+    "url": "/React-Portfolio/static/css/main.08325502.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdffbe546bb673795e5",
+    "url": "/React-Portfolio/static/js/main.3439ac75.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "c447cfe333c2c905cf17e19f27235f0a",
+    "url": "/React-Portfolio/static/media/Coding Quiz.c447cfe3.png"
+  },
+  {
+    "revision": "9d97a139990e74010d308776c4244f75",
+    "url": "/React-Portfolio/static/media/Day Planner.9d97a139.png"
+  },
+  {
+    "revision": "7d10b4e4b4a1a5c8a3e2f28d52023570",
+    "url": "/React-Portfolio/static/media/Flash Shopping.7d10b4e4.png"
+  },
+  {
+    "revision": "d317cda555aaa2b211a48b4329509f80",
+    "url": "/React-Portfolio/static/media/Food Tracker.d317cda5.png"
+  },
+  {
+    "revision": "fadff7512dbe7be52ee0aad6b7b0b028",
+    "url": "/React-Portfolio/static/media/Note Taker.fadff751.png"
+  },
+  {
+    "revision": "be3fb3f31e25f718e1c2908798dd793c",
+    "url": "/React-Portfolio/static/media/Password Generator.be3fb3f3.png"
+  },
+  {
+    "revision": "f4ddc33880b64f3d927a030c778b442c",
+    "url": "/React-Portfolio/static/media/Resume.f4ddc338.pdf"
+  },
+  {
+    "revision": "2dd1a574cf67fe1af82c3bb019b084ba",
+    "url": "/React-Portfolio/static/media/Tech blog.2dd1a574.png"
+  },
+  {
+    "revision": "37dba39be686d843e402f2cfdc8c3ab5",
+    "url": "/React-Portfolio/static/media/Weather Dashboard.37dba39b.png"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/React-Portfolio/static/media/cover-image.402a471b.jpg"
+  }
+]);
